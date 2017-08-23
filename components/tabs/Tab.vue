2a693606da61d2e61ed1deb1b13f24bd99bf5791
Sebastian Bechtold <sbecht@geomer.de>
2017-08-23 16:40:17 +0200

    Cleanup

--- components/tabs/Tab.vue
+++ components/tabs/Tab.vue
@@ -1,40 +1,21 @@
-<template>
-   
-    <div v-if="active" class="vuety-tab" role="tabpanel">
-
-        
-        <div>
-            <slot/>
-        </div>
-
+<template>   
+    <div v-if="active" class="vuety-tab" role="tabpanel">        
+        <div><slot/></div>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
-import Tabbed from './Tabbed.vue'
 
 @Component({
-
-
     props: {
-
         'title': String
     }
 })
 export default class Tab extends Vue {
-
     active: Boolean = false;
-
     title: string;   
 }
 </script>
-
-<style lang="scss">
-div.vuety-tab {
-    line-height:1.5em;
-
-}
-</style>
 
--- components/tabs/Tabbed.vue
+++ components/tabs/Tabbed.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="vuety-tabbed">
-
         <ul role="tablist">
             <li :class="tab.active ? 'active' : ''" v-for="(tab, index) in tabs" @click="onTabClick(tab)">
                 {{tab.title}}
@@ -16,18 +15,14 @@
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
-import uuidv4 from 'uuid/v4';
 import Tab from './Tab.vue';
 
 @Component({
     props: {
         'name': String
     }
-
-
 })
 export default class Tabbed extends Vue {
-
 
     tabs: Array<Tab> = [];
 
@@ -47,21 +42,14 @@ export default class Tabbed extends Vue {
         }
 
         tab.active = true;
-
-
     }
-
 }
 </script>
 
 <style lang="scss">
 $background: #f0f0f0;
-$tabs-base-color: #eee;
-
-$line: #88f;
 
 div.vuety-tabbed {
-
     
     >ul {
         list-style: none;
@@ -69,15 +57,12 @@ div.vuety-tabbed {
         margin: 0;
         padding: 0;
         >li {
-
-
             border-radius: 6px 6px 0 0;
             display: inline-block;
             cursor: pointer;
             padding: 10px 16px;
             position: relative;
             font-size: 1.2em;
-
             top: 8px;
         }
 
@@ -99,76 +84,5 @@ div.vuety-tabbed {
         padding: 20px;
     }
 }
-
-
-
-
-
-/*
-div.vuety-tabbed {
-    border: 1px solid #000;
-
-    position: relative;
-
-    >ul {
-        margin: 0;
-        padding: 0;
-
-        >li {
-            float: left;
-            display: block;
-
-            >input[type="radio"] {
-                display: none;
-            }
-
-            >input:checked+label {
-
-                top: 10px;
-                padding-bottom: 18px;
-                background: darken($tabs-base-color, 15);
-            }
-
-            >input:checked~[class^="tab-content"] {
-                display: block;
-            }
-
-            // This is the 'tab-content' div: 
-            >div {
-                z-index: 2;
-                display: none;
-                overflow: auto;
-                width: 100%;
-
-                position: absolute;
-
-
-                top: 53px;
-                left: 0;
-                background: darken($tabs-base-color, 15);
-            }
-
-
-            >label {
-                outline: none;
-                display: block;
-                padding: 8px 21px;
-                border-radius: 4px 4px 0 0;
-
-
-
-                background: $tabs-base-color;
-                cursor: pointer;
-                position: relative;
-                top: 20px;
-
-                &:hover {
-                    background: darken($tabs-base-color, 10);
-                }
-            }
-        }
-    }
-}
-*/
 </style>
 
