7bfc1a5f9312bed3f70a8633a76fc093bf1c6103
Sebastian Bechtold <sbecht@geomer.de>
2017-08-25 09:14:50 +0200

    Added LinearLayout component

---
+++ components/layout/LinearLayout.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="vuety-linear-layout">
+        <slot/>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component({
+    props: {
+
+    }
+})
+export default class LinearLayout extends Vue {
+
+}
+</script>
+
+<style lang="scss">
+div.vuety-linear-layout {
+    display: flex;
+    align-items: stretch;
+    height: 100%;
+
+    >* {
+        // 'width:100%'' is SUPER IMPORTANT here to ensure correct rendering in Firefox!!!
+        width: 100%;
+    }
+}
+</style>
