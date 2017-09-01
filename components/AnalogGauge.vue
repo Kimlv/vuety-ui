95fc698c8557bec105fe138fbb29c9295ef8ce12
Sebastian Bechtold <mr.bechtold@gmail.com>
2017-09-01 16:58:56 +0200

    Improved AnalogGauge

--- components/AnalogGauge.vue
+++ components/AnalogGauge.vue
@@ -4,7 +4,6 @@
         <circle cx="100" cy="100" r="100" fill="rgb(200,200,200)" />
 
         <line x1="0" y1="0" x2="-90" y2="0" style="stroke:rgb(0,0,0);stroke-width:4" :transform="needleTransform" />
-
     </svg>
 </template>
 
@@ -14,29 +13,57 @@ import { Component, Inject, Model, Prop, Vue, Watch } from 'vue-property-decorat
 @Component({})
 export default class AnalogGauge extends Vue {
 
+    //########## BEGIN Props ##########
+    @Prop({ default: 1 })
+    max : number;
+
+    @Prop({ default: 0 })
+    min : number;
+
     @Prop({ default: 0 })
     value: number;
+    //########## END Props ##########
 
+    updateIntervalHandle : number;
+    updateInterval_ms : number = 50;
 
-    needleAngle_deg: number = 0;
+    pNeedleAngle_deg: number = 0;
 
-    get needleTransform(): string {
+    maxAngle_deg : number = 180;
 
-        return "translate(100,100) rotate(" + this.needleAngle_deg + ")";
+    //############### BEGIN get/set properties #################
+    get needleAngle_deg() : number {
+        return this.pNeedleAngle_deg;
     }
 
+    set needleAngle_deg(v : number) {
+        this.pNeedleAngle_deg = Math.max(0, Math.min(v, this.maxAngle_deg));
+    }
 
-    mounted() {
+    
+    get needleTransform(): string {
+        return "translate(100,100) rotate(" + this.needleAngle_deg + ")";
+    }
+    //############### END get/set properties #################
 
-        window.setInterval(() => {
-            this.needleAngle_deg += (this.value - this.needleAngle_deg) * 0.1;
-        }, 50);
+    created() {
+
+        if (this.max <= this.min) {
+            console.warn("Vuety UI Analog Gauge: 'min' should be smaller than 'max'!");
+        }
+
+        // Set up update timer:
+        this.updateIntervalHandle = window.setInterval(() => {
+
+            let targetAngle = ((this.value - this.min) / (this.max - this.min)) * this.maxAngle_deg;
+
+            this.needleAngle_deg += (targetAngle - this.needleAngle_deg) * 0.1;
+        }, this.updateInterval_ms);
     }
 }
 </script>
 
 <style lang="scss">
-svg.vuety-analog-gauge {
-}
+
 </style>
 
