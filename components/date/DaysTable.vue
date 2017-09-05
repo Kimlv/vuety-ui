e76a096f345dd73385616b651273b36fe21fb782
Sebastian Bechtold <sbecht@geomer.de>
2017-09-05 16:35:53 +0200

    DaysTable: Fixed syntax error.

--- components/date/DaysTable.vue
+++ components/date/DaysTable.vue
@@ -29,7 +29,7 @@ import * as moment from 'moment'
 export default class DaysTable extends Vue {
 
     //######## BEGIN Props ########
-    @Prop({default : function() { return new Date(Date.now());})
+    @Prop({default : function() { return new Date(Date.now());} })
     date: Date;
 
     @Prop()
