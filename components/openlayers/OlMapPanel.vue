41d5c7bedfd9391b7f433749903a0b56abdb88f9
Sebastian Bechtold <sbecht@geomer.de>
2017-10-23 17:01:14 +0200

    Added OpenLayers map recording using RecordRTC

--- components/openlayers/OlMapPanel.vue
+++ components/openlayers/OlMapPanel.vue
@@ -3,45 +3,71 @@
 </template>
 
 <script lang="ts">
-import { Component, Inject, Model, Prop, Vue, Watch } from 'vue-property-decorator'
-
-
+import {
+  Component,
+  Inject,
+  Model,
+  Prop,
+  Vue,
+  Watch
+} from "vue-property-decorator";
 
 // Import OpenLayers code and CSS:
-import * as ol from 'openlayers'
-import 'openlayers/css/ol.css'
-
+import * as ol from "openlayers";
+import "openlayers/css/ol.css";
 
 @Component({})
 export default class OlMapPanel extends Vue {
-
-    //###### BEGIN Props #######
-    @Prop()
-    map: ol.Map;
-    //###### END Props #######
-
-    recordRTC: any;
-
-    created() {
-
-        // Workaround to fix map distortion when the map is embedded 
-        // in a flex container and the parent element is resized:
-        window.setInterval(() => {
-            this.map.updateSize();
-        }, 500);
-    }
+  //###### BEGIN Props #######
+  @Prop() map: ol.Map;
+  //###### END Props #######
 
 
-    mounted() {
-        // Attach map to targe div:
-        this.map.setTarget(<Element>this.$refs.mapDiv);
-    }
+  created() {
+    // Workaround to fix map distortion when the map is embedded
+    // in a flex container and the parent element is resized:
+    window.setInterval(() => {
+      this.map.updateSize();
+    }, 500);
+
+    
+  }
+
+  mounted() {
+    // Attach map to targe div:
+    this.map.setTarget(<Element>this.$refs.mapDiv);    
+/*
+    this.map.on('postcompose', (event : ol.events.Event) => {
+        
+        if (this.postcomposeCanvas != event.context.canvas) {
+            console.log("canvas update!");
+            this.postcomposeCanvas = event.context.canvas;
+
+            this.recorder = RecordRTC(this.postcomposeCanvas, {
+            type: 'canvas',
+            showMousePointer: true
+            });
+
+            this.recorder.startRecording();
+
+            window.setTimeout(() => {
+                this.recorder.stopRecording(function(url : string) {
+                    console.log(url);
+                    
+                });
+
+            }, 30000);
+
+        }    
+    });
+    */
+  }
 }
 </script>
 
 <style lang="scss">
 div.vuety-ol-map-panel {
-    // IMPORTANT!:    
-    height: 100%;
+  // IMPORTANT!:
+  height: 100%;
 }
 </style>
---
+++ components/openlayers/OlMapRecorder.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="vuety-ol-map-recorder">
+        
+        <button @click="onButtonStartClick" :disabled="recording">Start</button>
+        <button @click="onButtonStopClick" :disabled="!recording">Stop</button>
+
+        <p class="recording" v-if="recording">Aufzeichnung läuft...</p>
+
+        <h4>Bisherige Aufzeichnungen:</h4>
+        <ul v-for="(url,index) of videoUrls">
+            <li><a :href="url" target="new">Video {{index + 1}}</a></li>
+        </ul>
+                    
+    </div>
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Inject,
+  Model,
+  Prop,
+  Vue,
+  Watch
+} from "vue-property-decorator";
+
+// Import OpenLayers code and CSS:
+import * as ol from "openlayers";
+import "openlayers/css/ol.css";
+
+@Component({})
+export default class OlMapPanel extends Vue {
+  //###### BEGIN Props #######
+  @Prop() map: ol.Map;
+  //###### END Props #######
+
+  postcomposeCanvas: HTMLCanvasElement;
+
+  recorder: any;
+  recording: boolean = false;
+  videoUrls: Array<string> = [];
+
+  mounted() {
+    this.map.on("postcompose", (event: any) => {
+      if (this.postcomposeCanvas != event.context.canvas) {
+        console.log("canvas update!");
+        this.postcomposeCanvas = event.context.canvas;
+
+        if (typeof this.recorder != "undefined") {
+          this.recorder.stopRecording();
+        }
+
+        this.recorder = RecordRTC(this.postcomposeCanvas, {
+          type: "canvas",
+          showMousePointer: true
+        });
+      }
+    });
+  }
+
+  onButtonStartClick(evt: MouseEvent) {
+      this.recorder.reset();
+      
+    this.recorder.startRecording();
+    this.recording = true;
+    console.log("Recording started!");
+  }
+
+  onButtonStopClick(evt: MouseEvent) {
+    
+    
+    this.recorder.stopRecording((url: string) => {
+      this.recording = false;
+      console.log(url);
+      this.videoUrls.push(url);
+    });
+
+    console.log("Recording stopped!");
+  }
+}
+</script>
+
+<style lang="scss">
+div.vuety-ol-map-recorder {
+  padding: 8px;
+
+  p.recording {
+    color: #f00;
+  }
+  ul {
+  }
+}
+</style>
